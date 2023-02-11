5de5db41c4dee05ae93498ab8fae8f5896878276
Michael Thomas <mthomas@ketchbrookanalytics.com>
2023-02-10 22:36:12 -0500

    ETL cleanup

--- app.R
+++ app.R
@@ -17,66 +17,78 @@ options(scipen = 999)
 
 # Data Prep ---------------------------------------------------------------
 
-# Grab data from data.ct.gov API endpoint specific to this dataset
-DECD_data <- RSocrata::read.socrata("https://data.ct.gov/resource/a8k4-9euq.json")
+# # Grab data from data.ct.gov API endpoint specific to this dataset
+# url <- "https://data.ct.gov/resource/a8k4-9euq.json"
+# 
+# cols <- c(
+#   "amount_leveraged", 
+#   "company", 
+#   "county_1", 
+#   "fiscal_year", 
+#   "funding_source", 
+#   "grant_amount", 
+#   "loan_amount", 
+#   "industry", 
+#   "municipality", 
+#   "status", 
+#   "total_assistance", 
+#   "total_project_cost", 
+#   "longitude", 
+#   "latitude"
+# )
+# 
+# query <- paste0(
+#   url, "?",
+#   "$query=SELECT ", paste(cols, collapse = ", "), 
+#   " WHERE state = 'CT'"
+# )
+# 
+# DECD_data <- RSocrata::read.socrata(
+#   "https://data.ct.gov/resource/a8k4-9euq.json?state=CT"
+# )
 
 # Format some of the variable types to 'numeric', and 
 # correct spelling error in "East WIndsor" observations 
-DECD_data <- DECD_data %>% 
-  dplyr::mutate_at(.vars = c("amount_leveraged", 
-                             "grant_amount", 
-                             "loan_amount", 
-                             "total_assistance", 
-                             "total_project_cost" 
-  ), 
-  .funs = as.numeric) %>% 
-  dplyr::mutate(municipality = ifelse(municipality == "East WIndsor", 
-                                      "East Windsor", 
-                                      municipality))
-
-# Create empty variables for longitude & latitude
-DECD_data$longitude <- c(rep(NA, nrow(DECD_data)))
-DECD_data$latitude <- c(rep(NA, nrow(DECD_data)))
-
-# Fill the empty variables created above by parsing out the
-# list variable called "geocoded_location.coordinates
-for (i in 1:nrow(DECD_data)) { 
-  
-  DECD_data$longitude[i] <- ifelse(is.null(DECD_data$geocoded_location.coordinates[[i]]) == TRUE, 
-                                   NA, 
-                                   DECD_data$geocoded_location.coordinates[[i]][1])
-  
-  DECD_data$latitude[i] <- ifelse(is.null(DECD_data$geocoded_location.coordinates[[i]]) == TRUE, 
-                                  NA, 
-                                  DECD_data$geocoded_location.coordinates[[i]][2])
-  
-}
-
-# Filter only observations where the value for 'state' is "CT"
-# Select only desired variables
-# Group different variations of "Manufacturing Assistance Act" into
-# one general category
-DECD_data <- DECD_data %>% 
-  dplyr::filter(state == "CT") %>%
-  dplyr::select(amount_leveraged, 
-                company, 
-                county_1, 
-                fiscal_year, 
-                funding_source, 
-                grant_amount, 
-                loan_amount, 
-                industry, 
-                municipality, 
-                status, 
-                total_assistance, 
-                total_project_cost, 
-                longitude, 
-                latitude) %>% 
-  dplyr::mutate(funding_source = dplyr::case_when(
-    grepl("Manufacturing Assistance Act", funding_source) == TRUE ~ "Manufacturing Assistance Act", 
-    funding_source == "Small Business Express Program" ~ "Small Business Express Program", 
-    TRUE ~ "Other"
-  ))
+DECD_data <- readRDS("data.RDS") |> 
+  dplyr::mutate(
+    dplyr::across(
+      .cols = grant_amount:amount_leveraged,
+      .fns = function(x) as.integer(x)
+    )
+  ) |> 
+  dplyr::select(
+    company = company_name, 
+    fiscal_year, 
+    funding_source, 
+    status = job_obligation_status, 
+    grant_amount, 
+    loan_amount, 
+    total_assistance, 
+    total_project_cost, 
+    amount_leveraged, 
+    industry = business_industry, 
+    municipality, 
+    county_1, 
+    geocoded_location.coordinates
+  ) |> 
+  dplyr::mutate(
+    municipality = dplyr::if_else(
+      municipality == "East WIndsor", 
+      "East Windsor", 
+      municipality
+    ), 
+    funding_source = dplyr::case_when(
+      grepl("Manufacturing Assistance Act", funding_source) == TRUE ~ "Manufacturing Assistance Act", 
+      funding_source == "Small Business Express Program" ~ "Small Business Express Program", 
+      TRUE ~ "Other"
+    )
+  ) |> 
+  tidyr::hoist(
+    geocoded_location.coordinates,
+    latitude = list(2L),
+    longitude = list(1L)
+  ) |> 
+  dplyr::select(-geocoded_location.coordinates)
 
 
 # Build the UI ------------------------------------------------------------
@@ -84,53 +96,53 @@ DECD_data <- DECD_data %>%
 # Construct the sidebar
 sidebar <- shiny::div(id = "Sidebar",
                       shiny::sidebarPanel( 
-    shiny::h1("CT DECD ANALYZER"), 
-    hr(), 
-    # Create input selection for the 'fiscal_year'
-    shinyWidgets::pickerInput(
-      inputId = "MapYearSelection", 
-      label = "Select a Year", 
-      choices = sort(unique(DECD_data$fiscal_year), 
-                     decreasing = TRUE), 
-      selected = unique(DECD_data$fiscal_year), 
-      options = list(`actions-box` = TRUE), 
-      multiple = T
-    ), 
-    # Create input selection for 'funding_source'
-    shinyWidgets::pickerInput(
-      inputId = "MapFundingSourceSelection", 
-      label = "Select a Funding Source", 
-      choices = sort(unique(DECD_data$funding_source)), 
-      selected = "Manufacturing Assistance Act", 
-      options = list(`actions-box` = TRUE),
-      multiple = TRUE
-    ), 
-    # Create input selection for 'municipality'
-    shinyWidgets::pickerInput(
-      inputId = "CitySelection",
-      label = "Select a City",
-      choices = sort(unique(DECD_data$municipality)),
-      selected = unique(DECD_data$municipality),
-      options = list(`actions-box` = TRUE),
-      multiple = TRUE
-    ), 
-    hr(), 
-    br(), 
-    # Add text to the sidebar below the input selectors
-    shiny::h5("This interactive dashboard displays data reported by the Connecticut Department of Economic and Community Development."), 
-    shiny::h5("The DECD provides support for Connecticut business in a variety of ways, including grants and loans."), 
-    shiny::h5("This dashboard shows the businesses in Connecticut that received assistance from the DECD in the last decade."), 
-    shiny::h5("To learn more about the DECD or see the public dataset used, click on the respsective links below:"), 
-    shiny::uiOutput("DECDurl"), 
-    shiny::uiOutput("DECDdataset"), 
-    hr(), 
-    br(), 
-    br(), 
-    shiny::h6("This dashboard was developed by Mike Thomas using the R programming language and the Shiny development framework."), 
-    shiny::h6("This is an open project, and all of the app code can be found in the following Github repository:"), 
-    shiny::uiOutput("GithubLink"),
-    width = 3
-)
+                        shiny::h1("CT DECD ANALYZER"), 
+                        hr(), 
+                        # Create input selection for the 'fiscal_year'
+                        shinyWidgets::pickerInput(
+                          inputId = "MapYearSelection", 
+                          label = "Select a Year", 
+                          choices = sort(unique(DECD_data$fiscal_year), 
+                                         decreasing = TRUE), 
+                          selected = unique(DECD_data$fiscal_year), 
+                          options = list(`actions-box` = TRUE), 
+                          multiple = T
+                        ), 
+                        # Create input selection for 'funding_source'
+                        shinyWidgets::pickerInput(
+                          inputId = "MapFundingSourceSelection", 
+                          label = "Select a Funding Source", 
+                          choices = sort(unique(DECD_data$funding_source)), 
+                          selected = "Manufacturing Assistance Act", 
+                          options = list(`actions-box` = TRUE),
+                          multiple = TRUE
+                        ), 
+                        # Create input selection for 'municipality'
+                        shinyWidgets::pickerInput(
+                          inputId = "CitySelection",
+                          label = "Select a City",
+                          choices = sort(unique(DECD_data$municipality)),
+                          selected = unique(DECD_data$municipality),
+                          options = list(`actions-box` = TRUE),
+                          multiple = TRUE
+                        ), 
+                        hr(), 
+                        br(), 
+                        # Add text to the sidebar below the input selectors
+                        shiny::h5("This interactive dashboard displays data reported by the Connecticut Department of Economic and Community Development."), 
+                        shiny::h5("The DECD provides support for Connecticut business in a variety of ways, including grants and loans."), 
+                        shiny::h5("This dashboard shows the businesses in Connecticut that received assistance from the DECD in the last decade."), 
+                        shiny::h5("To learn more about the DECD or see the public dataset used, click on the respsective links below:"), 
+                        shiny::uiOutput("DECDurl"), 
+                        shiny::uiOutput("DECDdataset"), 
+                        hr(), 
+                        br(), 
+                        br(), 
+                        shiny::h6("This dashboard was developed by Mike Thomas using the R programming language and the Shiny development framework."), 
+                        shiny::h6("This is an open project, and all of the app code can be found in the following Github repository:"), 
+                        shiny::uiOutput("GithubLink"),
+                        width = 3
+                      )
 )
 
 # Construct the main dashboard
@@ -164,7 +176,7 @@ maindash <- shiny::mainPanel(
     )
   )
 )
-  
+
 # Piece the UI together
 ui <- shiny::fluidPage( 
   # Format the dashboard theme/color
@@ -210,11 +222,11 @@ server <- shiny::shinyServer(function(input, output, session) {
   
   # Create leaflet map
   output$Map <- leaflet::renderLeaflet({
-    leaflet() %>% 
-      leaflet::addProviderTiles(provider = providers$Esri) %>% 
-      leaflet::addCircleMarkers(data = DECD_data %>% 
-                                  dplyr::filter(fiscal_year %in% input$MapYearSelection) %>% 
-                                  dplyr::filter(funding_source %in% input$MapFundingSourceSelection) %>% 
+    leaflet::leaflet() |> 
+      leaflet::addProviderTiles(provider = providers$Esri) |> 
+      leaflet::addCircleMarkers(data = DECD_data |> 
+                                  dplyr::filter(fiscal_year %in% input$MapYearSelection) |> 
+                                  dplyr::filter(funding_source %in% input$MapFundingSourceSelection) |> 
                                   dplyr::filter(municipality %in% input$CitySelection), 
                                 lng = ~longitude, 
                                 lat = ~latitude, 
@@ -225,39 +237,39 @@ server <- shiny::shinyServer(function(input, output, session) {
   
   # Create bar chart
   output$BarChart <- shiny::renderPlot({
-   ggplot(DECD_data %>%
-            dplyr::filter(fiscal_year %in% input$MapYearSelection) %>%
-            dplyr::filter(funding_source %in% input$MapFundingSourceSelection) %>%
-            dplyr::filter(municipality %in% input$CitySelection) %>% 
-            dplyr::rename(City = municipality) %>% 
-            dplyr::select(fiscal_year, total_assistance, City),
-            aes(x = fiscal_year,
-                y = total_assistance, 
-                fill = City)) +
-     ggplot2::geom_bar(stat = "identity") + 
-     ggplot2::xlab("Year") + 
-     ggplot2::ylab("Sum of Total Assistance") + 
-     ggplot2::scale_y_continuous(labels = scales::comma)
-     
- })
-
+    ggplot2::ggplot(DECD_data |>
+             dplyr::filter(fiscal_year %in% input$MapYearSelection) |>
+             dplyr::filter(funding_source %in% input$MapFundingSourceSelection) |>
+             dplyr::filter(municipality %in% input$CitySelection) |> 
+             dplyr::rename(City = municipality) |> 
+             dplyr::select(fiscal_year, total_assistance, City),
+           ggplot2::aes(x = fiscal_year,
+               y = total_assistance, 
+               fill = City)) +
+      ggplot2::geom_bar(stat = "identity") + 
+      ggplot2::xlab("Year") + 
+      ggplot2::ylab("Sum of Total Assistance") + 
+      ggplot2::scale_y_continuous(labels = scales::comma)
+    
+  })
+  
   # Create data table
   output$Table <- DT::renderDataTable({
-   DECD_data %>%
-     dplyr::filter(fiscal_year %in% input$MapYearSelection) %>%
-     dplyr::filter(funding_source %in% input$MapFundingSourceSelection) %>%
-     dplyr::filter(municipality %in% input$CitySelection) %>%
-     dplyr::mutate(Company = company,
-                   County = county_1,
-                   City = municipality,
-                   Year = fiscal_year,
-                   Funding_Source = funding_source,
-                   Industry = industry,
-                   Grant_Amt = formatC(round(grant_amount), format = "f", big.mark = ",", drop0trailing = TRUE), 
-                   Loan_Amt = formatC(round(loan_amount), format = "f", big.mark = ",", drop0trailing = TRUE),
-                   Total_Amt = formatC(round(total_assistance), format = "f", big.mark = ",", drop0trailing = TRUE),
-                   Amt_Leveraged = formatC(round(amount_leveraged), format = "f", big.mark = ",", drop0trailing = TRUE),
-                   Status = status) %>%
+    DECD_data |>
+      dplyr::filter(fiscal_year %in% input$MapYearSelection) |>
+      dplyr::filter(funding_source %in% input$MapFundingSourceSelection) |>
+      dplyr::filter(municipality %in% input$CitySelection) |>
+      dplyr::mutate(Company = company,
+                    County = county_1,
+                    City = municipality,
+                    Year = fiscal_year,
+                    Funding_Source = funding_source,
+                    Industry = industry,
+                    Grant_Amt = formatC(round(grant_amount), format = "f", big.mark = ",", drop0trailing = TRUE), 
+                    Loan_Amt = formatC(round(loan_amount), format = "f", big.mark = ",", drop0trailing = TRUE),
+                    Total_Amt = formatC(round(total_assistance), format = "f", big.mark = ",", drop0trailing = TRUE),
+                    Amt_Leveraged = formatC(round(amount_leveraged), format = "f", big.mark = ",", drop0trailing = TRUE),
+                    Status = status) |>
       dplyr::select(Company,
                     County,
                     City,
